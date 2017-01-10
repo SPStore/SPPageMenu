08c2c554e24e64946587f868658706e4bb36179b
leshengping <lanou@YF-10.local>
2017-01-10 20:49:56 +0800

    大重构
    大重构

---
+++ Example/SPPageMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPPageMenu
+//
+//  Created by lanou on 16/12/17.
+//  Copyright © 2016年 leshengping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/SPPageMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SPPageMenu
+//
+//  Created by lanou on 16/12/17.
+//  Copyright © 2016年 leshengping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
